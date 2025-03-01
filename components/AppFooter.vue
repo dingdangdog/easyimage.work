2b01a4a4026263d27e9f32c469d5ea82057cfba2
dddx <dingdangdogx@outlook.com>
2025-03-01 20:49:55 +0800

    add contact email

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,11 +3,12 @@
     class="min-h-16 p-2 bg-gray-950 flex flex-col items-center justify-center space-y-2"
   >
     <p>
-      <strong
-        >Power By
-        <a href="https://easyimage.work" class="underline"
-          >easyimage.work</a
-        ></strong
+      Power By
+      <a href="https://easyimage.work" class="underline">easyimage.work</a>
+    </p>
+    <p>
+      <a href="mailto:contact@easyimage.work" class="underline"
+        >contact@easyimage.work</a
       >
     </p>
     <!-- <p>
