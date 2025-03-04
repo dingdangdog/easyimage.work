8312ab570726faad0e29ec77407cc21cc75a6f6f
dddx <dingdangdogx@outlook.com>
2025-03-04 18:40:59 +0800

    fix: enhance image preview with improved background and close buttons

--- components/image/preview.vue
+++ components/image/preview.vue
@@ -1,17 +1,28 @@
 <template>
   <div
     v-if="image"
-    class="fixed w-screen h-screen inset-0 bg-black bg-opacity-70 z-50"
+    class="fixed w-screen h-screen inset-0 bg-gray-800 bg-opacity-90 z-50"
     @keydown.esc="$emit('close-preview')"
     tabindex="0"
   >
     <div class="relative p-2 w-full h-full flex justify-center items-center">
       <img
         :src="image"
-        class="max-w-full max-h-full cursor-pointer"
-        @click="$emit('close-preview')"
+        class="max-w-full max-h-full cursor-pointer bg-gray-300/50"
         alt="Fullscreen Overview"
       />
+      <button
+        @click="$emit('close-preview')"
+        class="text-white bg-red-600/60 hover:bg-red-500/60 px-2 py-1 md:px-4 md:py-2 rounded-lg absolute top-2 left-2 md:top-4 md:left-4"
+      >
+        <IconClose class="w-4 h-4 md:w-6 md:h-6" color="rgb(243 244 246)" />
+      </button>
+      <button
+        @click="$emit('close-preview')"
+        class="text-white bg-red-600/60 hover:bg-red-500/60 px-2 py-1 md:px-4 md:py-2 rounded-lg absolute bottom-2 left-2 md:bottom-4 md:left-4"
+      >
+        <IconClose class="w-4 h-4 md:w-6 md:h-6" color="rgb(243 244 246)" />
+      </button>
       <button
         @click="$emit('close-preview')"
         class="text-white bg-red-600/60 hover:bg-red-500/60 px-2 py-1 md:px-4 md:py-2 rounded-lg absolute top-2 right-2 md:top-4 md:right-4"
